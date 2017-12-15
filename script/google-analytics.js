c5e6ecb11a4806b61c16ee17e4d10c5d5e9873c2
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2017-12-15 12:47:56 +0100

    fix(analytics): fix lint errors

--- script/google-analytics.js
+++ script/google-analytics.js
@@ -1,9 +1,11 @@
 function initGoogleAnalytics() {
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
 
-  gtag('config', 'UA-54179841-17');
+    gtag("config", "UA-54179841-17");
 }
 
 export { initGoogleAnalytics };
